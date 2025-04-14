b708c45b64de040c8e0c9c98adda0c4aeee5e3cf
Paxton M <paxtoncoghlin@gmail.com>
2025-04-14 13:28:20 -0400

    Added MEDIA + Schematic
    Code update for better readability

--- LED_Strips.ino
+++ LED_Strips.ino
@@ -1,202 +1,238 @@
+/*  ------------ V2 -----------
+ - Has both modes
+ - got rid of rgb values by % with numbers less than 1. It was stupid IMO
+
+------------- TODO -----------
+ - Improve code readability
+ - multiple commands in one message by parsing with a semicolon
+ - new function to just send a number of loops you want across the entire strip
+ - allow for motion sensor to turn then off automatically
+ - keep data through power cycles usng EEPROM
+}
+*/
+
 #include <BluetoothSerial.h>
 #include <Adafruit_NeoPixel.h>
+
 #define PIN 19
-#define NUM_PIXELS 300
+#define NUM_PIXELS 370
 #define MAX_BRIGHTNESS 200
 #define delayValue 10
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUM_PIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
 
-
 double stepSize = 0.07;
 String command = "";
 
-boolean enable [NUM_PIXELS];
-double colorCoefficient [] = {1, 1, 1};
-double colorValue [] = {0,0,0};
-double intensity;
+boolean enable[NUM_PIXELS];
+double colorValue[] = {0, 0, 0};
+double rgb[] = {100, 0, 200}; // 0 - 255
+
+int mode = 1; // mode 1 is tricolor, mode 2 has rgb controls
 
 double a = 0;
-double b = M_PI * 0.07;
+double b = M_PI * 0.01;
 double c = 75;
 double d = c;
 
-
 BluetoothSerial BluetoothDevice;
 
+void Send(String data) {
 
-void Send (String data) {
-
-    BluetoothDevice.print("\n");
-    for (int i = 0; i < data.length(); i++){
-        BluetoothDevice.print(data.charAt(i));
-    }
+  BluetoothDevice.print("\n");
+  for (int i = 0; i < data.length(); i++) {
+    BluetoothDevice.print(data.charAt(i));
+  }
 }
-boolean equals (String one, String two) {
 
-    if (one.length() == two.length()){
-        for (int i = 0; i < one.length(); i++){
-            if (one.charAt(i) != two.charAt(i)){
-                return false;
-            }
-        }
-    } else {
+boolean equals(String one, String two) {
+
+  if (one.length() == two.length()) {
+    for (int i = 0; i < one.length(); i++) {
+      if (one.charAt(i) != two.charAt(i)) {
         return false;
+      }
+    }
+  }
+  else {
+    return false;
+  }
+
+  return true;
+}
+
+void process(String info) { // readability is a 0/10 in this bad boy
+
+  if (equals(info.substring(0, 5), "step=")) { // command to change speed that sin wave moves
+    stepSize = info.substring(5).toDouble();
+
+  } else if (info.charAt(0) == 'a') { // sets horizontal position. little use unless step=0
+    a = info.substring(2).toDouble();
+
+  } else if (info.charAt(0) == 'b') { // horizontal stretch/compression
+    b = M_PI * info.substring(2).toDouble();
+
+  } else if (equals(info.substring(0, 7), "period=") == true) { // set period
+    b = (2 * M_PI) / info.substring(7).toDouble();
+
+  } else if (info.charAt(0) == 'c') { // vertical streth/compression
+    c = info.substring(2).toDouble();
+    d = c; // optional. I use this cause its easy
+
+  } else if (info.charAt(0) == 'd') { // displaces brightness up/down
+    d = c + info.substring(2).toDouble();
+
+  } else if (equals(info.substring(0, 7), "enable(") == true) { // enable/disable leds
+    if (info.indexOf("-") == -1) { // if only working with one light
+      enable[info.substring(info.indexOf('(') + 1, info.indexOf(')')).toInt()] = !enable[info.substring(info.indexOf('(') + 1, info.indexOf(')')).toInt()];
+    
+    } else { // change line of pixels
+      for (int i = info.substring(info.indexOf('(') + 1, info.indexOf('-')).toInt(); i < info.substring(info.indexOf('-') + 1, info.indexOf(')')).toInt(); i++) {
+        enable[i] = !enable[i];
+      }
     }
 
-    return true;
+  } else if (equals("read", info)) { // read current values from the board
+    Send("Mode = " + String(mode));
+    Send("Step = " + String(stepSize));
+    Send("a = " + String(a));
+    Send("b = " + String(b / M_PI));
+    Send("c = " + String(c));
+    Send("d = " + String(d));
 
+    if (mode == 2) {
+      Send("RGB = (" + String(rgb[0]) + ", " + String(rgb[1]) + ", " + String(rgb[2]) + ")");
+    }
+
+  } else if (equals("on", info)) { // enable all lights
+    for (int i = 0; i < NUM_PIXELS; i++) {
+      enable[i] = true;
+    }
+
+  } else if (equals("off", info)) { // disable all lights
+    for (int i = 0; i < NUM_PIXELS; i++) {
+      enable[i] = false;
+    }
+  
+  } else if (equals(info.substring(0, 4), "mode")) { // change control mode
+    mode = info.substring(5).toInt();
+  
+  } else if (equals(info.substring(0, 3), "rgb")) {
+
+    rgb[0] = (command.substring(command.indexOf('(') + 1, command.indexOf(','))).toDouble();
+    rgb[1] = (command.substring(command.indexOf(',') + 1, command.lastIndexOf(','))).toDouble();
+    rgb[2] = (command.substring(command.lastIndexOf(',') + 1, command.indexOf(')'))).toDouble();
+  }
 }
 
 void setup() {
 
-    pixels.begin();
-    pixels.show();
+  pixels.begin();
+  pixels.setPixelColor(3, 10, 10, 10); // see how long the loop after this takes without having pixels.show in it
+  pixels.show();
 
-    for (int i = 0; i < NUM_PIXELS - 1; i++){
-        enable[i] = true;
+  for (int i = 0; i < NUM_PIXELS; i++) {
+    enable[i] = true;
+    pixels.setPixelColor(i, 2, 2, 2);
+  }
+  pixels.show(); // this has been moved outside ^this^ loop
 
-        pixels.setPixelColor(i, pixels.Color(random(100), random(100), random(100)));
-        pixels.show();
-    }
-
-    Serial.begin(115200);
-    BluetoothDevice.begin("Ram Ranch Really Rocks"); // bluetooth device name
-
+  Serial.begin(115200);                  // idk if this is needed. Test
+  BluetoothDevice.begin("🍆FAT NUTS🍆"); // bluetooth device name
 }
 
 void loop() {
 
-    command = ""; // clear string to set it to new data
-
-    while(BluetoothDevice.available()){ // get messages from bluetooth
-        command += (char) BluetoothDevice.read();
+  while (BluetoothDevice.available()) { // get messages from bluetooth
+    command += (char)BluetoothDevice.read();
+  }
+  command.toLowerCase(); // set command to lower case
+  for (int i = 0; i < command.length(); i++) { // take out spacebar
+    if (command.charAt(i) == ' ') {
+      command.remove(i, 1);
+      i--;
     }
-    command.toLowerCase(); // set command to lower case
-     // take spacebar out of command
-    for (int i = 0; i < command.length(); i++) { // take out spacebar
-        if (command.charAt(i) == ' ') {
-            command.remove(i,1);
-            i--;
+  }
+
+  if (!command.equals("")) { // if command has contents
+    process(command); // use command
+    command = ""; // clear so we can use it on next read
+  }
+
+  for (int i = 0; i < NUM_PIXELS; i++) { // for all pixels
+
+    if (enable[i] == true) { // if pixel enabled
+
+      switch (mode) {
+      case 1: // TriColor Calculations
+
+        for (int j = 0; j < 3; j++) { // for all 3 colors
+
+          switch (j) { // huge calculation
+          case 0:
+            colorValue[j] = c * (cos(b * (i + a))) + d;
+            break;
+
+          case 1:
+            colorValue[j] = c * (cos(b * (i + a - (((2 * M_PI) / b) / 3)))) + d; // subtract one third of the period in order to keep waves off each other
+            break;
+
+          case 2:
+            colorValue[j] = c * (cos(b * (i + a + (((2 * M_PI) / b) / 3)))) + d;
+            break;
+
+          default: // how about you do absolutely nothing
+            Send("Something has gone terribly wrong");
+            break;
+          }
+
+          if (colorValue[j] > MAX_BRIGHTNESS) { // max brightness
+            colorValue[j] = MAX_BRIGHTNESS;
+          } else if (colorValue[j] < 0) { // min brightness
+            colorValue[j] = 0;
+          }
         }
-    }
+        break;
 
+      case 2:
+        for (int k = 0; k < 3; k++) { // scales color output depending on what the current color coefficients are
+          colorValue[k] = (c * (cos(b * (i + a))) + d) * (rgb[k] / 255);
 
-    if (!command.equals("")){ // if command has contents
-
-        if (equals(command.substring(0,5), "step=")){ // command to change speed that sin wave moves
-            stepSize = command.substring(5).toDouble();        
-
-        } else if (command.substring(0, command.indexOf('(')).equals("rgb")){ // change color brightness, if bigger than 1, treat as rgb value
-
-            if ((command.substring(command.indexOf('(') + 1, command.indexOf(','))).toDouble() > 1) {
-                colorCoefficient[0] = (command.substring(command.indexOf('(') + 1, command.indexOf(','))).toDouble() / 255;
-            } else {
-                colorCoefficient[0] = (command.substring(command.indexOf('(') + 1, command.indexOf(','))).toDouble();
-            }
-
-            if ((command.substring(command.indexOf(',') + 1, command.lastIndexOf(','))).toDouble() > 1) {
-                colorCoefficient[1] = (command.substring(command.indexOf(',') + 1, command.lastIndexOf(','))).toDouble() / 255;
-            } else {
-                colorCoefficient[1] = (command.substring(command.indexOf(',') + 1, command.lastIndexOf(','))).toDouble();
-            }
-
-            if ((command.substring(command.lastIndexOf(',') + 1, command.indexOf(')'))).toDouble() > 1) {
-                colorCoefficient[2] = (command.substring(command.lastIndexOf(',') + 1, command.indexOf(')'))).toDouble() / 255;
-            } else {
-                colorCoefficient[2] = (command.substring(command.lastIndexOf(',') + 1, command.indexOf(')'))).toDouble();
-            }
-                                                                    
-        } else if (command.charAt(0) == 'a'){
-            a = command.substring(2).toDouble();
-
-        } else if (command.charAt(0) == 'b'){
-            b = M_PI * command.substring(2).toDouble();
-
-        } else if (equals (command.substring(0,7), "period=") == true) { // set period
-            b = (2 * M_PI) / command.substring(7).toDouble();
-
-        } else if (command.charAt(0) == 'c'){
-            c = command.substring(2).toDouble();
-            d = c; // optional. I use this cause its easy
-        
-        } else if (command.charAt(0) == 'd') { // allows for constant colors, and to have none turned off
-            d = c + command.substring(2).toDouble();
-
-        } else if (equals (command.substring(0,7), "enable(") == true) { // enable/disable leds
-
-            if (command.indexOf("-") == -1) { // if only working with one light
-                enable [command.substring(command.indexOf('(') + 1, command.indexOf(')')).toInt()] = !enable [command.substring(command.indexOf('(') + 1, command.indexOf(')')).toInt()];
-
-            } else { // change line of them
-                for (int i = command.substring(command.indexOf('(') + 1, command.indexOf('-')).toInt(); i < command.substring(command.indexOf('-') + 1, command.indexOf(')')).toInt(); i++) {
-                    enable [i] = !enable [i];
-                }
-
-            }
-
-        } else if (equals("read", command)){ // read current values from the board
-
-            Send ("Red = " + String(colorCoefficient[0]));
-            Send ("Green = " + String(colorCoefficient[1]));
-            Send ("Blue = " + String(colorCoefficient[2]));
-
-            Send ("Step = " + String(stepSize));
-
-            Send ("a = " + String(a)); // Kinda useless since it changes so much
-            Send ("b = " + String(b / M_PI));
-            Send ("c = " + String(c));
-            Send ("d = " + String(d));
-
-        } else if (equals("on", command)) { // enable all lights
-
-            for (int i = 0; i < NUM_PIXELS; i++){
-                enable [i] = true;
-            }
-        
-        } else if (equals("off", command)) { // disable all lights
-        
-            for (int i = 0; i < NUM_PIXELS; i++){
-                enable [i] = false;
-            }
-        
-        }
-    }
-
-    for (int i = 0; i < NUM_PIXELS; i++){ // for all pixels
-
-        if (enable[i] == true) {
-            intensity = c * (cos(b * (i + a))) + d; // the big calculation
-
-
-            for (int i = 0; i < (sizeof(colorValue) / sizeof(double)); i++){ // scales color output depending on what the current color coefficients are
-                colorValue[i] = intensity * colorCoefficient[i];
-
-                if (colorValue[i] > MAX_BRIGHTNESS){ // max brightness
-                    colorValue[i] = MAX_BRIGHTNESS;
-                }else if (colorValue[i] < 0){ // lower limit
-                    colorValue[i] = 0;
-                }
-                
-            }
-
-            pixels.setPixelColor(i, pixels.Color(colorValue[0], colorValue[1], colorValue[2]));
-        } else {
-            pixels.setPixelColor(i, 0, 0, 0);
+          if (colorValue[k] > MAX_BRIGHTNESS) { // max brightness
+            colorValue[k] = MAX_BRIGHTNESS;
+          }
+          else if (colorValue[k] < 0) { // lower limit
+            colorValue[k] = 0;
+          }
         }
 
-    }
-    delay (delayValue);
-    pixels.show();
+        break;
 
-    a += stepSize; // change a value to cycle smoothly
-    if (a > M_PI * (1/b)){
-        a = ((M_PI * -1) * (1/b)) + stepSize;
-    }
+      default:
+        Send("Select a Valid Mode:");
+        Send("1 : TriColor");
+        Send("2 : RGB Control");
+        break;
+      }
 
+      pixels.setPixelColor(i, pixels.Color(colorValue[0], colorValue[1], colorValue[2]));
+    }
+    else { // if pixel disabled
+      pixels.setPixelColor(i, 0, 0, 0);
+    }
+  }
+  delay(delayValue);
+  pixels.show();
+
+  a += stepSize; // change a value to cycle smoothly
+  if (a > M_PI * (1 / b)) {
+    a = ((M_PI * -1) * (1 / b)) + stepSize;
+  
+  } else if (a < ((M_PI * -1) * (1 / b))) {
+    a = ((M_PI * (1 / b))) - stepSize; // set to big
+  }
 }
